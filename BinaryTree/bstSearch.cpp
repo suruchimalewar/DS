3564eddfa4b9ed7e0f356e702466c724c92ef9ed
suruchi <suru.malewar@gmail.com>
2018-08-19 16:47:28 +0530

    BST search

---
+++ BinaryTree/bstSearch.cpp
@@ -0,0 +1,39 @@
+#include "BinaryTree.h"
+#include "BST.h"
+#include <iostream>
+
+using namespace std;
+Node *SearchNode(Node *root, int val)
+{
+    if (root == nullptr)
+        return nullptr;
+    if (root->data == val)
+    {
+        return root;
+    }
+    if (root->data >= val)
+    {
+        return SearchNode(root->left, val);
+    }
+
+    return SearchNode(root->right, val);
+}
+int main()
+{
+    // Variation 2
+    Node *root = NULL;
+    for (int i = 0; i < 10; ++i)
+    {
+        BSTInsert(root, rand() % 50);
+    }
+
+    drawBinaryTree(root);
+    Inorder(root);
+    cout << endl;
+
+    Node *temp = SearchNode(root, 49);
+
+    cout << "found node" << temp->data;
+
+    return 0;
+}
